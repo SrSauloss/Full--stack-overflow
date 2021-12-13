ae8e778164b3a8a12abf30c63541f3f5b6ee034a
SrSauloss <saulosilva@super.ufam.edu.br>
2021-12-13 13:18:21 -0400

    Added possibility to answer a question

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,14 +1,22 @@
-import connection from "../database/database";
-import { db_user, user } from "../protocols/user.protocol";
+/* eslint-disable no-shadow */
+import connection from '../database/database';
+import { db_user, user } from '../protocols/user.protocol';
 
 async function findUser(user:user) : Promise<db_user> {
-    const resul = await connection.query(`SELECT * FROM users WHERE name = $1`,[user.name]);
-    return resul.rows[0];
+  const resul = await connection.query('SELECT * FROM users WHERE name = $1', [user.name]);
+  return resul.rows[0];
+}
+
+async function findUserByToken(token:string) {
+  const resul = await connection.query(`SELECT users.name, users.id FROM users
+    JOIN auths ON users.id = auths.user_id
+    WHERE auths.token = $1`, [token]);
+  return resul.rows[0];
 }
 
 async function storeUser(user:user) : Promise<db_user> {
-    const resul = await connection.query(`INSERT INTO users (name, class) VALUES($1, $2) RETURNING *`,[user.name, user.class]);
-    return resul.rows[0];
+  const resul = await connection.query('INSERT INTO users (name, class) VALUES($1, $2) RETURNING *', [user.name, user.class]);
+  return resul.rows[0];
 }
 
-export { storeUser,findUser };
+export { storeUser, findUser, findUserByToken };
--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -1,5 +1,7 @@
-import { db_question, question } from "../protocols/question.protocol";
+import QuestionError from "../errors/question.error";
+import { answer, db_answer, db_question, question } from "../protocols/question.protocol";
 import * as questionRepository from '../repositories/question.repository';
+import * as userRepository from '../repositories/user.repository';
 
 async function storeQuestion(objectQuestion : question) {
 
@@ -18,4 +20,26 @@ async function getQuestionById(id : string) {
     return res;
 }
 
-export { storeQuestion, getQuestionById };
+async function updateAnswerQuestion(token: string, id : string, answer : string) {
+    const question = await questionRepository.getQuestionById(id);
+
+    if(question.answered){
+        throw new QuestionError('This question has already been answered');
+    }
+
+    const resulUser = await userRepository.findUserByToken(token);
+
+    const objectAnswer : answer = { 
+        question_id: id, 
+        user_id: resulUser.id, 
+        answeredBy: resulUser.name,
+        answer: answer
+    }
+
+    const resultAnswer = await questionRepository.updateQuestion(objectAnswer);
+    console.log(resultAnswer)
+ 
+    return resultAnswer;
+}
+
+export { storeQuestion, getQuestionById, updateAnswerQuestion };
