b0806888a29c8a131b29be9cb1591efbdc849d3e
SrSauloss <saulosilva@super.ufam.edu.br>
2021-12-13 09:16:19 -0400

    Added validation schema

--- src/controllers/questions.controller.ts
+++ src/controllers/questions.controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
+import { questionStoreSchema } from '../validations/schemas';
 
 async function storeQuestion(req: Request, res: Response) {
+    const { question, student, tags } = req.body;
+    const classe = req.body.class;
+
+    const isValid = questionStoreSchema.validate({ question, student, tags, class: classe });
+    console.log(isValid.error);
     res.sendStatus(201);
 }
 
